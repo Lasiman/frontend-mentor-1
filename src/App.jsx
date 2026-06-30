fab839ab1f433212f245eff41c7c680fa0565e69
Hafizh <hafizhmiladurrohman121@gmail.com>
2026-06-30 11:22:36 +0700

    menambahkan custom style

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-amber-700'>
-      hallo
+    <div className='bg-sun-300 text-5xl'>
+      <p className='text-paragraph font-dmSans'>Hallo</p>
+      <p className='text-6xl font-dmMono'>Whereas</p>
+      <p className='text-6xl font-fraunces'>Hallo</p>
     </div>
   )
 }
