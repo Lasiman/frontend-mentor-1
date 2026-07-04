112566c9d85233644eadbbeb4c7b7132dfba28ee
Hafizh <hafizhmiladurrohman121@gmail.com>
2026-07-04 20:05:36 +0700

    done

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,7 +3,9 @@ import Header from './components/Header'
 import HeroContent from './components/HeroContent'
 import Card1 from './components/Card1'
 import Card2 from './components/Card2'
-import barcode from '../assets/icon-barcode.svg'
+import key from'./assets/icon-key.svg'
+import wifi from './assets/icon-wifi.svg'
+import breakfast from './assets/icon-breakfast.svg'
 
 const Hero = () => {
     return (
@@ -13,10 +15,25 @@ const Hero = () => {
                 <Card1></Card1>
                 <Card2></Card2>
             </section>
-            <div>
-                <HeroContent nama='ARRIVAL' no='01' image='../assets/icon-key.svg' judul='Check-in from 15:00' teks='Sat, 25 April' variant='red'>a;izsajg;rpoaifvj</HeroContent>
-                <link src='../assets/icon-key.svg' className='bg-black'/>
-                <img src={barcode}/>
+            <div className='mt-15 flex gap-10 max-mobile:flex-col'>
+                <HeroContent nama='ARRIVAL' no='01' image={key} judul='Check-in from 15:00' teks='Sat, 25 April' variant='red'>
+                    <p className='font-dmSans text-neutral-700 text-lg font-medium'>Ring the brass bell by the blue door. if we're at the market, the key is in the terracotta pot by the olive tree</p>
+                </HeroContent>
+                <HeroContent nama='WIFI' no='02' image={wifi} judul='Le Soleil • Guest' teks='Password below' variant='blue'>
+                    <div>
+                        <div className='bg-neutral-300 rounded-lg flex items-center justify-between px-3 py-1'>
+                            <p className='text-neutral-500 font-dmMono text-lg'>NETWORK</p>
+                            <p>le Soleil - guest</p>
+                        </div>
+                        <div className='bg-neutral-300 rounded-lg flex items-center justify-between px-3 py-1 mt-3'>
+                            <p className='text-neutral-500 font-dmMono text-lg'>PASSWORD</p>
+                            <p>soleil-2026</p>
+                        </div>
+                    </div>
+                </HeroContent>
+                <HeroContent nama='BREAKFAST' no='03' image={breakfast} judul='Served 8 - 10:30' teks='On the terrace, 25 April' variant=''>
+                    <p className='font-dmSans text-neutral-700 text-lg font-medium'>Fresh figs, Mardeille honey, pain au levain, and espresso. gluten-free option? Leave a note the night before</p>
+                </HeroContent>
 
             </div>
         </section>
--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 
 const HeroContent = ({children, nama, no, image, judul, teks, variant}) => {
 
-    const gambar = image
     return (
-        <div>
-            <div className={`${variant === 'red' ? 'text-terracotta-600' : variant === 'blue' ? 'text-blue-500' : 'text-rose-500'}`}>
-                <div>
-                    <div>
-                        <img src={gambar} />
+        <div className='border border-neutral-300 bg-neutral-0 p-3 rounded-2xl min-h-70 shadow-xl flex-1'>
+            <div className={`${variant === 'red' ? 'text-terracotta-600' : variant === 'blue' ? 'text-blue-500' : 'text-rose-500'} flex items-center justify-between`}>
+
+                <div className='flex items-center gap-5'>
+                    <div className={`${variant === 'red' ? 'bg-terracotta-600' : variant === 'blue' ? 'bg-blue-500' : 'bg-rose-500'} rounded-lg p-2 w-fit`}>
+                        <img src={image} className='m-auto w-7'/>
                     </div>
-                    <p></p>
+                    <p className='font-dmMono text-lg'>{nama}</p>
                 </div>
-                <p></p>
+
+                <p className='font-fraunces text-2xl font-semibold'>{no}</p>
+
             </div>
-            <div></div>
+
+
+            <div>
+                <h1 className='font-fraunces text-3xl mb-2 mt-5'>{judul}</h1>
+                <p className='text-neutral-500 font-dmSans mb-5'>{teks}</p>
+            </div>
             {children}
         </div>
     )
